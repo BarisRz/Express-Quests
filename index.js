be1c5557487d2d018ec4bfc8c0346af423e60b50
Baris G <barisg@pm.me>
2023-11-27 15:54:14 +0100

    fini

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
 const app = require("./src/app");
 
-const port = 5000;
+const { APP_PORT } = process.env;
+
+const port = APP_PORT;
 
 app
   .listen(port, () => {
-    console.log(`Server is listening on ${port}`);
+    console.log(`Server is listening on ${port} http://localhost:${port}`);
   })
   .on("error", (err) => {
     console.error("Error:", err.message);
